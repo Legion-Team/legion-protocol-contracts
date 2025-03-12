b8aadedd7586ce6a97e7d23b8837d64ce59abc47
0xHustling <0xhustling@proton.me>
2025-03-12 23:39:22 +0200

    Update NatSpec for `LegionPreLiquidSaleV2Factory.sol`

--- src/factories/LegionPreLiquidSaleV2Factory.sol
+++ src/factories/LegionPreLiquidSaleV2Factory.sol
@@ -23,33 +23,34 @@ import { ILegionSale } from "../interfaces/sales/ILegionSale.sol";
 import { ILegionPreLiquidSaleV2Factory } from "../interfaces/factories/ILegionPreLiquidSaleV2Factory.sol";
 import { ILegionPreLiquidSaleV2 } from "../interfaces/sales/ILegionPreLiquidSaleV2.sol";
 import { LegionPreLiquidSaleV2 } from "../sales/LegionPreLiquidSaleV2.sol";
+
 /**
  * @title Legion Pre-Liquid Sale V2 Factory
  * @author Legion
  * @notice A factory contract for deploying proxy instances of Legion pre-liquid V2 sales
+ * @dev Utilizes the clone pattern to create new instances of LegionPreLiquidSaleV2 contracts
  */
-
 contract LegionPreLiquidSaleV2Factory is ILegionPreLiquidSaleV2Factory, Ownable {
     using LibClone for address;
 
-    /// @dev The LegionPreLiquidSaleV2 implementation contract
+    /// @notice Address of the LegionPreLiquidSaleV2 implementation contract used as template
+    /// @dev Immutable reference to the base implementation deployed during construction
     address public immutable preLiquidSaleV2Template = address(new LegionPreLiquidSaleV2());
 
     /**
-     * @dev Constructor to initialize the LegionSaleFactory
-     *
-     * @param newOwner The owner of the factory contract
+     * @notice Initializes the LegionPreLiquidSaleV2Factory with an owner
+     * @dev Sets up ownership during contract deployment
+     * @param newOwner Address to be set as the initial owner of the factory
      */
     constructor(address newOwner) {
         _initializeOwner(newOwner);
     }
 
     /**
-     * @notice Deploy a LegionPreLiquidSaleV2 contract.
-     *
-     * @param saleInitParams The Legion sale initialization parameters.
-     *
-     * @return preLiquidSaleV2Instance The address of the preLiquidSaleV2Instance deployed.
+     * @notice Deploys a new LegionPreLiquidSaleV2 contract instance
+     * @dev Clones the template contract and initializes it with provided parameters; restricted to owner
+     * @param saleInitParams Struct containing Legion sale initialization parameters
+     * @return preLiquidSaleV2Instance Address of the newly deployed and initialized PreLiquidSaleV2 instance
      */
     function createPreLiquidSaleV2(ILegionSale.LegionSaleInitializationParams memory saleInitParams)
         external
