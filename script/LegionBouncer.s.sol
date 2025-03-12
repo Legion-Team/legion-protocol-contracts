72a21c1a59dbb5e4881b719b670e1efff03ff0dd
0xHustling <0xhustling@proton.me>
2025-03-12 14:48:15 +0200

    refactor: Update Solidity version to 0.8.29

--- script/LegionBouncer.s.sol
+++ script/LegionBouncer.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.29;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { LegionBouncer } from "../src/access/LegionBouncer.sol";
