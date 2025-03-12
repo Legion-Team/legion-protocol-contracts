004a79cf2fd20862e4b94160e24bccab6e084a9f
0xHustling <0xhustling@proton.me>
2025-03-12 23:52:17 +0200

    Update NatSpec for `LegionAddressRegistry.sol`

--- src/interfaces/registries/ILegionAddressRegistry.sol
+++ src/interfaces/registries/ILegionAddressRegistry.sol
@@ -17,32 +17,34 @@ pragma solidity 0.8.29;
 // We will pay a fair bounty for any issue that puts users' funds at risk.
 
 /**
- * @title Legion Address Registry Interface
- * @notice An interface for managing Legion Protocol addresses
+ * @title ILegionAddressRegistry
+ * @author Legion
+ * @notice Interface for managing Legion Protocol addresses
+ * @dev Defines events and functions for address registry operations
  */
 interface ILegionAddressRegistry {
     /**
      * @notice Emitted when a Legion address is set or updated
-     *
-     * @param id The unique identifier of the address
-     * @param previousAddress The previous address before the update
-     * @param updatedAddress The updated address
+     * @dev Provides details about address changes in the registry
+     * @param id Unique identifier (bytes32) of the address
+     * @param previousAddress Address previously associated with the identifier
+     * @param updatedAddress New address associated with the identifier
      */
     event LegionAddressSet(bytes32 id, address previousAddress, address updatedAddress);
 
     /**
-     * @notice Sets a Legion address
-     *
-     * @param id The unique identifier of the address
-     * @param updatedAddress The new address to set
+     * @notice Sets a Legion address for a given identifier
+     * @dev Must be implemented to update the address registry
+     * @param id Unique identifier (bytes32) of the address
+     * @param updatedAddress New address to associate with the identifier
      */
     function setLegionAddress(bytes32 id, address updatedAddress) external;
 
     /**
-     * @notice Gets a Legion address
-     *
-     * @param id The unique identifier of the address
-     * @return The registered Legion address
+     * @notice Retrieves the Legion address associated with a given identifier
+     * @dev Must be implemented to return the registered address for the specified id
+     * @param id Unique identifier (bytes32) of the address
+     * @return Registered Legion address associated with the identifier
      */
     function getLegionAddress(bytes32 id) external view returns (address);
 }
--- src/registries/LegionAddressRegistry.sol
+++ src/registries/LegionAddressRegistry.sol
@@ -24,25 +24,27 @@ import { ILegionAddressRegistry } from "../interfaces/registries/ILegionAddressR
  * @title Legion Address Registry
  * @author Legion
  * @notice A contract used to maintain the state of all addresses used in the Legion Protocol
+ * @dev Manages a mapping of unique identifiers to addresses for the Legion ecosystem; restricted to owner
  */
 contract LegionAddressRegistry is ILegionAddressRegistry, Ownable {
-    /// @dev Mapping of unique identifiers to Legion addresses
+    /// @notice Mapping of unique identifiers to their corresponding Legion addresses
+    /// @dev Stores the registry state as a private mapping
     mapping(bytes32 => address) private _legionAddresses;
 
     /**
-     * @dev Constructor to initialize the LegionAddressRegistry
-     *
-     * @param newOwner The owner of the registry contract
+     * @notice Initializes the LegionAddressRegistry with an owner
+     * @dev Sets up ownership during contract deployment
+     * @param newOwner Address to be set as the initial owner of the registry
      */
     constructor(address newOwner) {
         _initializeOwner(newOwner);
     }
 
     /**
-     * @notice Sets a Legion address for the given identifier
-     *
-     * @param id The unique identifier for the address
-     * @param updatedAddress The new address to set
+     * @notice Sets a Legion address for a given identifier
+     * @dev Updates the address mapping and emits an event; only callable by the owner
+     * @param id Unique identifier (bytes32) for the address
+     * @param updatedAddress New address to associate with the identifier
      */
     function setLegionAddress(bytes32 id, address updatedAddress) external onlyOwner {
         // Cache the previous address before update
@@ -56,10 +58,10 @@ contract LegionAddressRegistry is ILegionAddressRegistry, Ownable {
     }
 
     /**
-     * @notice Returns the Legion address for the given identifier
-     *
-     * @param id The unique identifier for the address
-     * @return The registered Legion address
+     * @notice Retrieves the Legion address associated with a given identifier
+     * @dev Returns the address stored in the mapping for the specified id
+     * @param id Unique identifier (bytes32) for the address
+     * @return Registered Legion address associated with the identifier
      */
     function getLegionAddress(bytes32 id) public view returns (address) {
         return _legionAddresses[id];
