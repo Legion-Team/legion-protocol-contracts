2894b8a7e64eb00940d05c729c4557b29c09abb4
0xHustling <0xhustling@proton.me>
2025-03-12 14:51:31 +0200

    refactor: Update Solidity version to 0.8.29 (#8)

--- script/LegionPreLiquidSaleV2Factory.s.sol
+++ script/LegionPreLiquidSaleV2Factory.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.29;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { LegionPreLiquidSaleV2Factory } from "../src/factories/LegionPreLiquidSaleV2Factory.sol";
